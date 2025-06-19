b1d9b1b0c3484fcf37bcf1f60cf32d74c0c287fa
Pavlo Serikov <113522310+rel1c-hub@users.noreply.github.com>
2025-06-19 18:30:11 +0300

    feature : basic input validation

--- src/main/java/notimpressed/devs/productcataloguecachesystem/model/Product.java
+++ src/main/java/notimpressed/devs/productcataloguecachesystem/model/Product.java
@@ -1,6 +1,7 @@
 package notimpressed.devs.productcataloguecachesystem.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -21,12 +22,19 @@ public class Product {
     @Column(nullable = false)
     private String name;
 
+    @Size(max = 255, message = "Description can't be longer than 256 characters.")
     private String description;
 
+    @Column(nullable = false)
+    @NotNull(message = "Price can't be null. Please enter the price.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
 
+    @NotBlank(message = "Category can't be blank. Please enter the category.")
     private String category;
 
+    @NotNull(message = "Stock can't be null. Please enter the stock value.")
+    @Min(value = 0, message = "Stock can't be negative")
     private Integer stock;
 
     private LocalDateTime createdDate;
