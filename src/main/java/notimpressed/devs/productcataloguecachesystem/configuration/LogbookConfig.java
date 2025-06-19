a4372325468fb11fbcea6a238336f479c905923f
Pavlo Serikov <113522310+rel1c-hub@users.noreply.github.com>
2025-06-19 23:36:18 +0300

    fix : swagger excluded from logging

--- src/main/java/notimpressed/devs/productcataloguecachesystem/configuration/LogbookConfig.java
+++ src/main/java/notimpressed/devs/productcataloguecachesystem/configuration/LogbookConfig.java
@@ -1,21 +1,31 @@
 package notimpressed.devs.productcataloguecachesystem.configuration;
 
+import org.zalando.logbook.HttpRequest;
 import org.zalando.logbook.Logbook;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.core.CurlHttpLogFormatter;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
-
+import java.util.function.Predicate;
 
 @Configuration
 public class LogbookConfig {
 
     @Bean
     public Logbook logbook() {
+        Predicate<HttpRequest> isSwagger = request -> {
+            String path = request.getPath();
+            return path.startsWith("/swagger-ui")
+                    || path.startsWith("/v3/api-docs")
+                    || path.startsWith("/v3/swagger-config");
+        };
+
         return Logbook.builder()
+                .condition(isSwagger.negate())
                 .sink(new DefaultSink(
-                        new CurlHttpLogFormatter(), new DefaultHttpLogWriter()
+                        new CurlHttpLogFormatter(),
+                        new DefaultHttpLogWriter()
                 ))
                 .build();
     }
