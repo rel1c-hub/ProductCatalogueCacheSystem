78a8e4f3e85a59b1a3ae426b78ed482f1c38d299
Pavlo Serikov <113522310+rel1c-hub@users.noreply.github.com>
2025-06-19 16:03:08 +0300

    feature : initial commit of a project

---
+++ src/test/java/notimpressed/devs/productcataloguecachesystem/ProductCatalogueCacheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package notimpressed.devs.productcataloguecachesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogueCacheSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
